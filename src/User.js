38a5b528d972a910bf2d5c20db748b396b438b1b
hershieljay <144136463+hershieljay@users.noreply.github.com>
2023-11-16 19:59:06 +0800

    Add files via upload

---
+++ src/User.js
@@ -0,0 +1,123 @@
+export const User = [
+  {
+    id: 1,
+    name: {
+      firstname: "Anna",
+      lastname: "Lopez",
+    },
+    course: "BSIT",
+    yearsection: "3A",
+    username: "annalopez@gmail.com",
+    password: "1111",
+    usertype: "STUDENT",
+  },
+  {
+    id: 2,
+    name: {
+      firstname: "Bea",
+      lastname: "Sanchez",
+    },
+    course: "BSIT",
+    yearsection: "3A",
+    username: "beasanchez@gmail.com",
+    password: "2222",
+    usertype: "STUDENT",
+  },
+  {
+    id: 3,
+    name: {
+      firstname: "Catherine",
+      lastname: "Flores",
+    },
+    course: "BSIT",
+    yearsection: "3A",
+    username: "catherine@gmail.com",
+    password: "3333",
+    usertype: "STUDENT",
+  },
+  {
+    id: 4,
+    name: {
+      firstname: "Fatima",
+      lastname: "Sabang",
+    },
+    course: "BSIT",
+    yearsection: "3A",
+    username: "fatimasabang@gmail.com",
+    password: "4444",
+    usertype: "STUDENT",
+  },
+  {
+    id: 5,
+    name: {
+      firstname: "Eunice",
+      lastname: "PAJOTA",
+    },
+    course: "BSIT",
+    yearsection: "3A",
+    username: "eunicepajota@gmail.com",
+    password: "5555",
+    usertype: "STUDENT",
+  },
+  {
+    id: 6,
+    name: {
+      firstname: "Mark",
+      lastname: "TADLAS",
+    },
+    course: "BSIT",
+    yearsection: "3A",
+    username: "KENNETH@gmail.com",
+    password: "6666",
+    usertype: "STUDENT",
+  },
+  {
+    id: 7,
+    name: {
+      firstname: "JERRY",
+      lastname: "ALE",
+    },
+    course: "",
+    yearsection: "",
+    username: "JERRY@gmail.com",
+    password: "7777",
+    usertype: "TEACHER",
+  },
+  {
+    id: 8,
+    name: {
+      firstname: "GABRINE",
+      lastname: "DIAZ",
+    },
+    course: "",
+    yearsection: "",
+    username: "GABRINE@gmail.com",
+    password: "8888",
+    usertype: "TEACHER",
+  },
+  {
+    id: 9,
+    name: {
+      firstname: "FERMO",
+      lastname: "ASUFRA",
+    },
+    course: "",
+    yearsection: "",
+    username: "FERMO@gmail.com",
+    password: "9999",
+    usertype: "TEACHER",
+  },
+  {
+    id: 10,
+    name: {
+      firstname: "WENZEAL",
+      lastname: "AGOC",
+    },
+    course: "",
+    yearsection: "",
+    username: "WENZEAL@gmail.com",
+    password: "1010",
+    usertype: "TEACHER",
+  },
+  
+];
